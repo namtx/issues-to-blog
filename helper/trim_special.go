8d26185961078c01b7caf53cc2cbd2361d50084e
Nam Tran <nam.tran.ext@personio.de>
2021-02-20 17:51:15 +0700

    bifurcation

--- helper/trim_special.go
+++ helper/trim_special.go
@@ -2,7 +2,7 @@ package helper
 
 import "strings"
 
-var replacer = strings.NewReplacer("/", "_")
+var replacer = strings.NewReplacer("/", "_", " ", "_")
 
 func TrimSpecial(source string) string {
 	return replacer.Replace(source)
--- main.go
+++ main.go
@@ -13,57 +13,68 @@ func main() {
 	accessToken := "33871a9f95079a27b2f395e3e57010cb948b826d"
 	owner := "namtx"
 	sourceRepository := "til"
+	targetRepository := "issues-to-blog"
 	targetBranch := "main"
 	targetDirectory := "issues"
-	ctx := context.Background()
+	ownerEmailAddress := "namtx.93@gmail.com"
 
+	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: accessToken},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
+	
+
+	refs, _, err := client.Git.GetRef(ctx, owner, targetRepository, "refs/heads/"+targetBranch)
+	if (err != nil) {
+		panic(err.Error())
+	}
+	entries := []*github.TreeEntry{}
+
 	issues, _, err := client.Issues.ListByRepo(ctx, owner, sourceRepository, nil)
 	if (err != nil) {
 		panic(err.Error())
 	}
-
-	refs, _, err := client.Git.GetRef(ctx, owner, "issues-to-blog", "refs/heads/"+targetBranch)
-	if (err != nil) {
-		panic(err.Error())
-	}
-
-	entries := []*github.TreeEntry{}
-
 	for _, issue := range issues {
 		entries = append(
 			entries, 
 			&github.TreeEntry{
-				Path: github.String(targetDirectory + helper.ToSnakeCase(helper.TrimSpecial(*issue.Title))), 
+				Path: github.String(targetDirectory+"/"+ helper.ToSnakeCase(helper.TrimSpecial(*issue.Title))+".md"), 
 				Type: github.String("blob"), Content: github.String(*issue.Body), 
 				Mode: github.String("100644"),
 			},
 		)
 	}
 
-	tree, _, err := client.Git.CreateTree(ctx, owner, "issues-to-blog", *refs.Object.SHA, entries)
+	tree, _, err := client.Git.CreateTree(ctx, owner, targetRepository, *refs.Object.SHA, entries)
 	if (err != nil) {
 		panic(err.Error())
 	}
-	parent, _, err := client.Repositories.GetCommit(ctx, owner, "issues-to-blog", *refs.Object.SHA)
+	parent, _, err := client.Repositories.GetCommit(ctx, owner, targetRepository, *refs.Object.SHA)
 	if (err != nil) {
 		panic(err.Error())
 	}
 	parent.Commit.SHA = parent.SHA
 
 	date := time.Now()
-	author := &github.CommitAuthor{Date: &date, Name: github.String("namtx"), Email: github.String("namtx.93@gmail.com")}
-	commit := &github.Commit{Author: author, Message: github.String("test commit"), Tree: tree, Parents: []*github.Commit{parent.Commit}}
+	author := &github.CommitAuthor{
+		Date: &date, 
+		Name: github.String(owner), 
+		Email: github.String(ownerEmailAddress),
+	}
+	commit := &github.Commit{
+		Author: author, 
+		Message: github.String(""), 
+		Tree: tree, 
+		Parents: []*github.Commit{parent.Commit},
+	}
 
-	newCommit, _, err := client.Git.CreateCommit(ctx, owner, "issues-to-blog", commit)
+	newCommit, _, err := client.Git.CreateCommit(ctx, owner, targetRepository, commit)
 
 	refs.Object.SHA = newCommit.SHA
-	_, _, err = client.Git.UpdateRef(ctx, owner, "issues-to-blog", refs, false)
+	_, _, err = client.Git.UpdateRef(ctx, owner, targetRepository, refs, true)
 	if (err != nil) {
 		panic(err.Error())
 	}
